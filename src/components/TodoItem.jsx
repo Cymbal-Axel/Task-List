3847119c33c37da515be39d82bdea10e244c7153
Axel <cymbalaxel@gmail.com>
2022-09-22 19:58:14 -0300

    recibe por parametro los datos de toggleTodo pasados atributo en TodoList y se le pasa a TodoItem

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 
-export function TodoItem({ todo }){
+export function TodoItem({ todo, toggleTodo }){
     const {id, task, completed} = todo
 
     return(
         <li>
-            <input type="checkbox"/>
+            <input type="checkbox" onChange={}/>
             {task}
         </li>
     )
