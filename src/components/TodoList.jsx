6e8c905c0c6ab009bf2552b028e0777ef41fd2c8
Axel <cymbalaxel@gmail.com>
2022-09-22 19:56:43 -0300

    se paso por propiedad la funcion toggleTodo al componente TodoList

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import { TodoItem } from './TodoItem';
 
-export function TodoList({todos}){
+export function TodoList({todos, toggleTodo}){
     console.log(todos)
     return(
         <ul>
             {todos.map((todo)=>(
-                <TodoItem key={todo.id} todo={todo}/>
+                <TodoItem key={todo.id} todo={todo} toggleTodo={toggleTodo}/>
             ))}
         </ul>
     )
